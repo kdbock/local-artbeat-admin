eb7b9810780bf4df037aa5f9277c3f75cebe386e
Kristy Kelly <kristy@kristykelly.com>
2025-11-29 16:42:17 -0500

    database table

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://kristykelly.com/api/:path*',
+        source: "/api/:path*",
+        destination: "https://kristykelly.com/api/:path*",
       },
     ];
   },
